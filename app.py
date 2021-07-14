ef3f68277efef16e4a50a991178380bd511c5566
DanielMichael82 <danielmpearce@live.co.uk>
2021-07-14 12:37:49 +0000

    Setup json data for about and owner information

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -18,7 +17,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-@app.route("/index")
+@app.route("/")
 def index():
     data = []
     with open("data/lilaharz.json", "r") as json_data:
