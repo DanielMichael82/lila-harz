c06edadf154986e056f66feb25fb4e35d2b8c5f9
DanielMichael82 <danielmpearce@live.co.uk>
2021-07-21 11:09:53 +0000

    connected sign up page to mongodb

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import os
 import json
 from flask import (
     Flask, flash, render_template,
-    request)
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -61,9 +62,27 @@ def login():
     return render_template("login.html", page_title="Login")
 
 
-@app.route("/coasters")
-def coasters():
-    return render_template("coasters.html", page_title="Coasters")
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("sign_up"))
+
+        sign_up = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Sign Up Successful!")
+    return render_template("sign_up.html")
 
 
 if __name__ == "__main__":
